6e23d35891d43bc4c020976b997b3ce1674e9b16
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2023-12-02 21:11:57 +0700

    tìm kiếm, sửa thông tin người dùng

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -11,57 +11,59 @@ class Product2 {
   bool? star;
   bool antiwater;
 
-  Product2({
-    this.id,
-    this.user_id,
-    required this.name,
-    required this.price,
-    this.discount,
-    this.thumbnail,
-    this.desc,
-    this.star,
-    required this.antiwater
-  });
+  Product2(
+      {this.id,
+      this.user_id,
+      required this.name,
+      required this.price,
+      this.discount,
+      this.thumbnail,
+      this.desc,
+      this.star,
+      required this.antiwater});
 
   Map<String, dynamic> toJSON() => {
-    'id': id,
-    'user_id': user_id,
-    'name': name,
-    'price': price,
-    'discount': discount,
-    'thumbnail': thumbnail,
-    'desc': desc,
-    'star': star,
-    'antiwater': antiwater,
-  };
+        'id': id,
+        'user_id': user_id,
+        'name': name,
+        'price': price,
+        'discount': discount,
+        'thumbnail': thumbnail,
+        'desc': desc,
+        'star': star,
+        'antiwater': antiwater,
+      };
 
   static Product2 fromJSON(Map<String, dynamic> json) => Product2(
-    id: json['id'],
-    user_id: json['user_id'],
-    name: json['name'],
-    price: json['price'],
-    discount: json['discount'],
-    thumbnail: json['thumbnail'],
-    desc: json['desc'],
-    star: json['star'],
-    antiwater: json['antiwater'],
-  );
+        id: json['id'],
+        user_id: json['user_id'],
+        name: json['name'],
+        price: json['price'],
+        discount: json['discount'],
+        thumbnail: json['thumbnail'],
+        desc: json['desc'],
+        star: json['star'],
+        antiwater: json['antiwater'],
+      );
 
   Map<String, dynamic> toFirestore() {
     return {
       if (id != null) "id": id,
       if (user_id != null) "user_id": user_id,
+      //if (name != null) "name": name,
+      //if (price != null) "price": price,
       if (discount != null) "discount": discount,
       if (thumbnail != null) "thumbnail": thumbnail,
       if (desc != null) "desc": desc,
       if (star != null) "star": star,
+      //if (antiwater != null) "antiwater": antiwater,
     };
   }
 
   factory Product2.fromFirestore(
-      DocumentSnapshot<Map<String, dynamic>> snapshot,
-      SnapshotOptions? options,
-      ) {
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
     final data = snapshot.data();
     return Product2(
       id: data?['id'],
